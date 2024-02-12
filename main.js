4b19c0c1cd2d1323cf9f7fc205cb9575014b419a
haoyuan <132324403+QMLOJ@users.noreply.github.com>
2024-02-12 14:58:17 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,96 +1,18 @@
-// 定义全局变量 res
-let res;
-
-// URL 缩短函数
-function shorturl() {
-  // 检查输入框是否为空
-  if (document.querySelector("#text").value == "") {
-    alert("URL不能为空！");
-    return;
-  }
-
-  // 禁用缩短按钮，并显示加载状态
-  document.getElementById("searchbtn").disabled = true;
-  document.getElementById("searchbtn").innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>请稍候...';
-
-  // 发送 POST 请求
-  fetch(window.location.pathname, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ url: document.querySelector("#text").value })
-  }).then(function (response) {
-    return response.json();
-  })
-    .then(function (myJson) {
-      res = myJson;
-      // 恢复缩短按钮状态和文本
-      document.getElementById("searchbtn").disabled = false;
-      document.getElementById("searchbtn").innerHTML = ' 缩短';
-      // 如果返回的 key 不为空，显示缩短后的 URL，并弹出结果弹窗
-      if (res.key !== "")
-        document.getElementById("result").innerHTML = window.location.host + res.key;
-      $('#exampleModal').modal('show');
-    }).catch(function (err) {
-      // 异常处理
-      alert("未知错误，请重试！");
-      console.log(err);
-      // 恢复缩短按钮状态和文本
-      document.getElementById("searchbtn").disabled = false;
-      document.getElementById("searchbtn").innerHTML = ' 缩短';
+let ip = "www.tyser-play.bf";
+let _ = "你真懒，5位数数字都有复制，那还要什么办法呢，复制了";
+let port = "20812";
+let qun_hao = "399153472";
+let abcd = "尽情期待";
+function copyTextToClipboard(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      console.log('文本已成功复制到剪贴板');
+    })
+    .catch(err => {
+      console.error('复制失败:', err);
     });
-}
-
-// 复制 URL 函数
-function copyurl(id, attr) {
-  let target = null;
-
-  if (attr) {
-    // 创建临时元素
-    target = document.createElement('div');
-    target.id = 'tempTarget';
-    target.style.opacity = '0';
-    if (id) {
-      let curNode = document.querySelector('#' + id);
-      target.innerText = curNode[attr];
-    } else {
-      target.innerText = attr;
-    }
-    document.body.appendChild(target);
-  } else {
-    target = document.querySelector('#' + id);
-  }
-
-  try {
-    // 复制操作
-    let range = document.createRange();
-    range.selectNode(target);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
-    console.log('复制成功');
-  } catch (e) {
-    console.log('复制失败');
-  }
-
-  if (attr) {
-    // 移除临时元素
-    target.parentElement.removeChild(target);
-  }
-}
-
-// 初始化 Bootstrap 弹出框
-$(function () {
-  $('[data-toggle="popover"]').popover();
-});
-
-// 打印 GitHub 项目链接到控制台
-console.log("https://github.com/xyTom/Url-Shorten-Worker/");
-
-// 通知信息
-let notice = "注意：此服务仅供演示目的，生成的短链接将在 24 小时后自动过期。";
-
-// 如果当前域名为 lnks.eu.org，显示通知信息
-if (window.location.host == "lnks.eu.org") {
-  document.getElementById("notice").innerHTML = notice;
-}
+};
+function a() {
+  copyTextToClipboard(port);
+  alert(_);
+};
